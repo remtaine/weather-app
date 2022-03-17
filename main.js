d0a9e6f29facb9c842e52e779cb7a358ebdb9fb1
Benjo Kho <benjokho@gmail.com>
2022-03-17 15:39:49 +0800

    Add search functionality

--- main.js
+++ main.js
@@ -1,6 +1,68 @@
+//api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=57c048c9f23d40a5a79648f61d282f79
+const searchButton = document.getElementById("search-country");
+const textBox = document.getElementById("search-term");
+
 const country = document.getElementById("country");
 const description = document.getElementById("description");
 const temperature = document.getElementById("temperature");
-const feeling = document.getElementById("feeling");
+const feels_like = document.getElementById("feels-like");
 const humidity = document.getElementById("humidity");
 const wind = document.getElementById("wind");
+
+const BLANK = "";
+const API_KEY = "57c048c9f23d40a5a79648f61d282f79";
+const searchTerm = "London,uk";
+// const searchTerm = "ssdf";
+
+const tempUsed = "Fahrenheit";
+const openWeather = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+searchButton.addEventListener("click", searchLocation);
+// searchButton.addEventListener("submit", searchLocation);
+
+input.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+    event.preventDefault();
+      // Trigger the button element with a click
+    searchButton.click();
+    }
+}); 
+
+function searchLocation() {
+    // console.log(textBox.value);
+    // const loc = searchTerm;
+    const loc = textBox.value;
+    const api_url = openWeather + loc + "&APPID=" + API_KEY;
+    fetch(api_url, {mode: 'cors'})
+        .then(function(response) {
+            return (response.json());
+        }).then(function(response) {
+            console.log(response);
+            console.log(response.name);
+            country.textContent = (response.name);
+
+            console.log(response.weather[0].description);
+            description.textContent = (response.weather[0].description);
+
+            console.log(response.main.temp);
+            temperature.textContent = (response.main.temp) + " " + tempUsed;
+            
+            console.log(response.main.feels_like);
+            feels_like.textContent = "Feels like: " + (response.main.feels_like) + " " + tempUsed;
+
+            console.log(response.main.humidity);
+            humidity.textContent = "Humidity: " + (response.main.humidity);
+
+            console.log(response.wind.speed);
+            wind.textContent = "Wind: " + (response.wind.speed) + "km/h";
+        }).catch(function(error) {
+            country.textContent = ("Location not found");
+            description.textContent = (BLANK);
+            temperature.textContent = (BLANK);
+            feels_like.textContent = (BLANK);
+            humidity.textContent = (BLANK);
+            wind.textContent = (BLANK);
+        });
+}
