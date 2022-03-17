f93f05ab52422982c2ee78fc5635513dd9452d04
Benjo Kho <benjokho@gmail.com>
2022-03-17 16:13:58 +0800

    Refactor

--- main.js
+++ main.js
@@ -20,16 +20,6 @@ const openWeather = "http://api.openweathermap.org/data/2.5/weather?q=";
 searchButton.addEventListener("click", searchLocation);
 // searchButton.addEventListener("submit", searchLocation);
 
-input.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-    event.preventDefault();
-      // Trigger the button element with a click
-    searchButton.click();
-    }
-}); 
-
 function searchLocation() {
     // console.log(textBox.value);
     // const loc = searchTerm;
