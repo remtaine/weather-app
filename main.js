e702e03c3e5b5ddcdebd3f946039c3769ad613cc
Benjo Kho <benjokho@gmail.com>
2022-03-17 14:41:59 +0800

    Add constants

---
+++ main.js
@@ -0,0 +1,6 @@
+const country = document.getElementById("country");
+const description = document.getElementById("description");
+const temperature = document.getElementById("temperature");
+const feeling = document.getElementById("feeling");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
